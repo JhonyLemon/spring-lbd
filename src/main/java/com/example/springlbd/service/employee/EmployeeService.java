6561dccc62559f629592ec9da0126e339fee5c24
Adam N <76851495+JhonyLemon@users.noreply.github.com>
2022-07-05 11:45:58 +0200

    Punkt 13

--- src/main/java/com/example/springlbd/service/employee/EmployeeService.java
+++ src/main/java/com/example/springlbd/service/employee/EmployeeService.java
@@ -3,9 +3,11 @@ package com.example.springlbd.service.employee;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
+import java.util.logging.Logger;
 
 @Service
 public interface EmployeeService {
+
     List findAll();
     String GetEmployeeNickname(String firstName, String lastName);
 }
--- src/main/java/com/example/springlbd/service/employee/EmployeeServiceOne.java
+++ src/main/java/com/example/springlbd/service/employee/EmployeeServiceOne.java
@@ -1,5 +1,7 @@
 package com.example.springlbd.service.employee;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Primary;
@@ -12,6 +14,8 @@ import java.util.List;
 @Profile("dev")
 public class EmployeeServiceOne implements EmployeeService {
 
+
+    private static final Logger LOG = LoggerFactory.getLogger(EmployeeServiceOne.class);
     @Value("${prefix}")
     String prefix;
     @Value("${sufix}")
@@ -24,8 +28,11 @@ public class EmployeeServiceOne implements EmployeeService {
 
     @Override
     public String GetEmployeeNickname(String firstName, String lastName) {
+        LOG.info("firstNmae: "+firstName);
+        LOG.info("lastName: "+lastName);
         String nickname=null;
         nickname = prefix+firstName.substring(0,(firstName.length()<3 ? firstName.length() : 3))+sufix+ lastName.substring(0,(lastName.length()<3 ? lastName.length() : 3));
+        LOG.info("nickname: "+nickname);
         return nickname;
     }
 }
