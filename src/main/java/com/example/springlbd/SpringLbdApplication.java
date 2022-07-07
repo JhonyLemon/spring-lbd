352fa7c065403505afc3b83945bb4a24b47d6de7
Adam N <76851495+JhonyLemon@users.noreply.github.com>
2022-07-07 10:16:43 +0200

    Punkt 1

--- src/main/java/com/example/springlbd/SpringLbdApplication.java
+++ src/main/java/com/example/springlbd/SpringLbdApplication.java
@@ -7,21 +7,7 @@ import javax.annotation.PostConstruct;
 
 @SpringBootApplication
 public class SpringLbdApplication {
-
-
-    private EmployeeService employeeService;
-
-    public SpringLbdApplication(EmployeeService employeeService) {
-        this.employeeService = employeeService;
-    }
-
     public static void main(String[] args) {
         SpringApplication.run(SpringLbdApplication.class, args);
     }
-
-    @PostConstruct
-    private void ExampleFindAllUsage(){
-        employeeService.findAll();
-    }
-
 }
--- src/test/java/com/example/springlbd/SpringLbdApplicationTests.java
+++ src/test/java/com/example/springlbd/SpringLbdApplicationTests.java
@@ -1,13 +1,7 @@
 package com.example.springlbd;
 
-import com.example.springlbd.enity.employee.Employee;
-import org.assertj.core.api.AssertionsForClassTypes;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
-
-import java.util.List;
 
 import static net.bytebuddy.matcher.ElementMatchers.is;
 import static org.hamcrest.Matchers.notNullValue;
